f6593b40079beb39a54318e62aac8e40f9b356bf
Barry Alexander <barry.alexander@gmail.com>
2022-06-15 21:08:32 -0700

    new contract id

--- constants/index.js
+++ constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = "0x2D2e8830fb3F16F7695efB0BfCdfFe7984F7e7Cc";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://learnweb3-nft-dapp.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
